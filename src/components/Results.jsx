b983c306e9c8686268bd4512b184e6923b7fa7af
Rivera G Alejandro <alejandro110498@hotmail.com>
2025-09-16 16:51:57 -0500

    Modificacion del Ganador - Game Over

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -4,12 +4,20 @@ import { useLocation, useNavigate } from "react-router-dom";
 function Results() {
     const { state } = useLocation();
     const navigate = useNavigate();
-    const winner = state?.winner;
+
+    const winnerId = state?.winner;
+
+    
+    const currentUser = JSON.parse(localStorage.getItem("user"));
+    const winnerName =
+        currentUser && currentUser.id === winnerId
+            ? currentUser.name
+            : winnerId;
 
     return (
         <div className="card">
             <h2>Resultados</h2>
-            <p>Ganador: {winner || "Desconocido"}</p>
+            <p>Ganador: {winnerName || "Desconocido"}</p>
             <button onClick={() => navigate("/lobby")}>Volver al Lobby</button>
         </div>
     );
