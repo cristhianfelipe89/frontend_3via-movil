7b12cd2484862a1c96d435103c16027a4bc4b53a
sajoesor <sajoesor@gmail.com>
2025-09-20 13:56:48 -0500

    Ajusto direccionamiento de back

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api", // Cambiar según backend desplegado
+    //baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api", // Cambiar según backend desplegado
+    baseURL: import.meta.env.VITE_API_URL || "http://api.prouni.online/api", // Cambiar según backend desplegado
 });
 
 export default api;
