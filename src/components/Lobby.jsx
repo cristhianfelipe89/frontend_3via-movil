70ea7bd7cae694a4f7f29443f2b6af94af50a2d1
sajoesor <sajoesor@gmail.com>
2025-09-27 17:21:18 -0500

    prueba

--- src/components/Lobby.jsx
+++ src/components/Lobby.jsx
@@ -68,7 +68,7 @@ function Lobby({ user, onStartGame, onLogout }) {
     return (
         <div className="card">
             <h2>🎮 Lobby</h2>
-            <p>Bienvenido Perrito{currentUser?.name || "Jugador"} 👋</p>
+            <p>Bienvenido 2{currentUser?.name || "Jugador"} 👋</p>
             <p>Esperando jugadores...</p>
 
             {typeof countdown === "number" && (
